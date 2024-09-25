1ddb1d0f723cb170dcfb681c5cf1fc4b291f25f3
ShriNarayanPandey01 <119402642+ShriNarayanPandey01@users.noreply.github.com>
2024-09-25 17:51:17 +0530

    Time: 320 ms (44.52%), Space: 187.3 MB (67.74%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
@@ -2,42 +2,40 @@ import java.util.*;
 
 class Trie {
     Trie[] child;
-    int[] alp;
-    char letter;
+    int score;
 
     public Trie() {
         child = new Trie[26];
-        alp = new int[26];
+        score = 0;
     }
 }
 
 class Solution {
     public int[] sumPrefixScores(String[] words) {
         Trie root = new Trie();
-        for (String word : words) {
-            insertWord(root, word);
-        }
         int[] result = new int[words.length];
-        for (int i = 0; i < words.length; i++) {
-            result[i] = countScore(root, words[i], 0);
-        }
-        return result;
-    }
 
-    public void insertWord(Trie root, String word) {
-        Trie current = root;
-        for (char c : word.toCharArray()) {
-            if (current.child[c - 'a'] == null) {
-                current.child[c - 'a'] = new Trie();
+        for (String word : words) {
+            Trie current = root;
+            for (char c : word.toCharArray()) {
+                if (current.child[c - 'a'] == null) {
+                    current.child[c - 'a'] = new Trie();
+                }
+                current = current.child[c - 'a'];
+                current.score++;
             }
-            current.alp[c - 'a']++;
-            current = current.child[c - 'a'];
         }
-    }
 
-    public int countScore(Trie root, String word, int index) {
-        if (index == word.length()) return 0;
-        char c = word.charAt(index);
-        return root.alp[c - 'a'] + countScore(root.child[c - 'a'], word, index + 1);
+        for (int i = 0; i < words.length; i++) {
+            Trie current = root;
+            int score = 0;
+            for (char c : words[i].toCharArray()) {
+                current = current.child[c - 'a'];
+                score += current.score;
+            }
+            result[i] = score;
+        }
+
+        return result;
     }
 }
