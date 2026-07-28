b6bc7552d2900266bd54f4492c48d24d718fc8d3
ShriNarayanPandey01 <119402642+ShriNarayanPandey01@users.noreply.github.com>
2026-07-29 02:48:09 +0530

    Time: 3 ms (99.46%), Space: 46.8 MB (67.14%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-
+    int[] arr ;
+    int arrInd;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         for( int i = 0 ; i< numCourses ; i++) graph.add( new ArrayList<>());
@@ -8,33 +9,27 @@ class Solution {
             graph.get(i[0]).add(i[1]);
             in[i[1]]++;
         }
-        ArrayList<Integer> ret = new ArrayList<>();
-        boolean[] vis = new boolean[numCourses];
+
+        arr = new int[numCourses];
+        arrInd = numCourses-1;
         for( int i = 0 ; i < numCourses ; i++){
             if( in[i] == 0 ){
-                ret.add(i);
-                if(!help(graph , i , in  , ret))
+                arr[arrInd--] = i;
+                if(!help(graph , i , in ))
                     return new int[]{};
             }
         }
-        int[] arr = ret.stream().mapToInt(Integer::intValue).toArray();
-        for (int i = 0; i < arr.length / 2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1 - i] = temp;
-        }
-        if( arr.length != numCourses)
-            return new int[]{};
+        if( arrInd != -1) return new int[]{};
         return arr;
     }
-    public boolean help(ArrayList<ArrayList<Integer>> graph , int ind , int[] in , ArrayList<Integer> ret){
+    public boolean help(ArrayList<ArrayList<Integer>> graph , int ind , int[] in ){
         
         for( int i : graph.get(ind)){
             if( in[i] == 0  ) return false;
             if( --in[i] == 0){
                 in[i]--;
-                ret.add(i);
-                if(!help(graph , i , in  , ret))
+                arr[arrInd--] = i;
+                if(!help(graph , i , in ))
                     return false;
             }           
         }
