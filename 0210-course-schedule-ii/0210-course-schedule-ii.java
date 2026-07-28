93c5d8ccf7d7fb8fbdd3d9ba74b91ec8a6816731
ShriNarayanPandey01 <119402642+ShriNarayanPandey01@users.noreply.github.com>
2026-07-29 02:39:35 +0530

    Time: 7 ms (28.54%), Space: 48.4 MB (6.88%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public HashSet<Integer> v = new HashSet<>();
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for( int i = 0 ; i< numCourses ; i++) graph.add( new ArrayList<>());
+        int[] in = new int[numCourses];
+        for( int[] i : prerequisites){
+            graph.get(i[0]).add(i[1]);
+            in[i[1]]++;
+        }
+        ArrayList<Integer> ret = new ArrayList<>();
+        boolean[] vis = new boolean[numCourses];
+        for( int i = 0 ; i < numCourses ; i++){
+            if( in[i] == 0 && !v.contains(i)){
+                ret.add(i);
+                v.add(i);
+                if(!help(graph , i , in  , ret))
+                    return new int[]{};
+            }
+        }
+        int[] arr = ret.stream().mapToInt(Integer::intValue).toArray();
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        if( arr.length != numCourses)
+            return new int[]{};
+        return arr;
+    }
+    public boolean help(ArrayList<ArrayList<Integer>> graph , int ind , int[] in , ArrayList<Integer> ret){
+        
+        for( int i : graph.get(ind)){
+            if( in[i] == 0  ) return false;
+            in[i]--;
+            if( in[i] == 0){
+                ret.add(i);
+                v.add(i);
+                if(!help(graph , i , in  , ret))
+                    return false;
+            }
+            
+        }
+        if( in[ind] == 0)
+            return true;
+        return false;
+    }
+}
