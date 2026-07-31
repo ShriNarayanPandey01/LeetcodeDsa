e975d43271898ebb46c8b6d24a3f1990f103e3c1
ShriNarayanPandey01 <119402642+ShriNarayanPandey01@users.noreply.github.com>
2026-07-31 23:08:37 +0530

    Time: 10 ms (80.2%), Space: 48.8 MB (76.56%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,41 +1,42 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int[][] adj = new int[n][n];
-        for (int[] row : adj) {
-            Arrays.fill(row, -1); // Initialize with a large value representing infinity
-        }
-        int[] time = new int[n];
-        Arrays.fill(time,10000000);
-        
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
+        int[] dist = new int[n];
+        Arrays.fill(dist , Integer.MAX_VALUE);
+        dist[k-1] = 0;
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+        for( int i =0 ; i < n ; i++) graph.add( new ArrayList<>());
 
-        for(int i = 0 ; i < times.length ; i ++){
-            adj[times[i][0] - 1][times[i][1] -1] = times[i][2];
+        for( int[] i : times){
+            graph.get(i[0]-1).add( new int[]{i[1]-1,i[2]});
         }
-        boolean[] vis = new boolean[n];
-        time[k - 1] = 0;
-        q.add(new int[] {0 , k - 1});
-        while(q.size()>0){
-            int[] pop = q.poll();
-            int cost = pop[0];
-            int src = pop[1];
-            if(vis[src]) continue;
-            vis[src] = true;
-            time[src] = cost;
-            for(int i = 0 ; i < n ; i ++){
-                if(adj[src][i] > -1){
-                    if(time[i] > cost + adj[src][i]){
-                        time[i] = cost + adj[src][i];
-                        q.offer(new int[]{time[i] ,i});
-                    }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a , int[] b){
+                return a[1] - b[1];
+            }
+        }); 
+        
+        pq.offer(new int[]{k-1,0});
+        while(pq.size() > 0){
+            int[] p = pq.poll();
+
+            for(int[] arr : graph.get(p[0]) ){
+                if( dist[p[0]] + arr[1] < dist[arr[0]]  ){
+                    dist[arr[0]] = dist[p[0]] + arr[1];
+
+                    pq.offer( new int[]{arr[0] ,dist[p[0]] + arr[1] });
                 }
             }
         }
         int max = -1;
-        for(int i : time){
-            if(i == 10000000) return -1;
-            max = Math.max(max , i);
-        }
-        return max;
+        for( int i : dist)
+            max = Math.max(i,max);
+        
+        return max == Integer.MAX_VALUE ? -1 : max;
+
+
     }
+
+
 }
